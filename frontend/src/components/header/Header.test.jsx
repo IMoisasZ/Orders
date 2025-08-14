d4f8b04a4b52d48717d71d3afeb18fa1e440ca03
moisas <mopri08@gmail.com>
2025-08-14 14:18:01 -0300

    included the first test of header after the fixed the configuration the jest.

--- frontend/src/components/header/Header.test.jsx
+++ frontend/src/components/header/Header.test.jsx
@@ -4,10 +4,14 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
+import '@testing-library/jest-dom'
 import Header from './Header'
 import { linksHeader } from '../../data/header_links.data'
 
-// Remova todos os blocos de jest.mock daqui
+jest.mock('./Header.module.css', () => ({
+	app_header: 'app-header-class',
+	header_logo: 'header-logo-class',
+}))
 
 describe('Header Component', () => {
 	test('deve renderizar todos os links de navegação', () => {
